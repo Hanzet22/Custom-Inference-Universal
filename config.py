d7cb8661742a8b789edb893c19730651ce863fe6
海鹏 鸟神 ft GFSC/Pejuang Silat X Negara <123471495+Hanzet22@users.noreply.github.com>
2026-06-24 12:52:02 +0700

    Masukkan Teks
    YOONSO-LQWA-ERQF-KURH

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ INPUT_DIR = Path("inputs")
 RESULT_DIR = Path("results")
 
 # Telegram (ambil dari environment variable, atau set default kosong)
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+TELEGRAM_BOT_TOKEN = os.getenv("8665685237:AAFj60ckefci50U1st8EJBPYu5FcMaZ28kY", "")
+TELEGRAM_CHAT_ID = os.getenv("7307931676", "")
 
 # Default model (untuk fallback jika tidak terdeteksi)
 DEFAULT_SCALE = 4
